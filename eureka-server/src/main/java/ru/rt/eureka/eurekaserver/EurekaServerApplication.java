3df06390194b96f519b6785b911eb91c52960632
Aleksey Вaidin <alekseybaidinq@gmail.com>
2021-06-26 00:02:48 +0500

    настройка микросервисов в docker, пока не коннектятся euruka сервер и клиенты

--- eureka-server/src/main/java/ru/rt/eureka/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/ru/rt/eureka/eurekaserver/EurekaServerApplication.java
@@ -2,12 +2,26 @@ package ru.rt.eureka.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+// (exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
+
+    @Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests().anyRequest().permitAll()
+                        .and()
+                    .csrf().disable();
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
