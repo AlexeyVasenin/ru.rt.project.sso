e15a98f78043c857c6600e6e01e29f08b077a8bd
Aleksey Вaidin <alekseybaidinq@gmail.com>
2021-07-08 23:34:15 +0500

    настроил распределенный механизм custom backchannel logout: перекрывается сессия через ...protocol/openid-connect/logout запрос, после создаются запросы ко всем остальным микросервисам для выполнения backchannel logout; пока не работает single-sign-out, поскольку кука JSESSIONID не удаляется; даже с мертвой OIDC-сессией, сервис ведет себя так, будто бы пользователь до сих пор авторизован

--- microservice-cinema/src/main/java/ru/rt/cinema/controllers/CinemaController.java
+++ microservice-cinema/src/main/java/ru/rt/cinema/controllers/CinemaController.java
@@ -3,14 +3,18 @@ package ru.rt.cinema.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.reactive.function.client.WebClient;
 import ru.rt.cinema.domain.Movie;
 
@@ -36,10 +40,12 @@ public class CinemaController {
     @GetMapping("/model")
     public String getAllFilms(Model model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String sessionId = ((WebAuthenticationDetails) authentication.getDetails()).getSessionId();
         DefaultOidcUser p = (DefaultOidcUser) authentication.getPrincipal();
         model.addAttribute("email", p.getClaims().get("email"));
         model.addAttribute("authorities",
                 p.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining(", ")));
+        model.addAttribute("sessionId", sessionId);
 
         List<Movie> movies = this.webClient.get()
                 .uri(cinemaApiUrl)
@@ -51,8 +57,17 @@ public class CinemaController {
         return "model";
     }
 
-    @GetMapping("/logout")
-    public void logout(HttpServletRequest request) throws ServletException {
+    @GetMapping("/test")
+    public String getTestPage() {
+        return "test";
+    }
+
+    @GetMapping("/back-channel-logout")
+    public String logout(@RequestParam String issuer, @RequestParam String token, HttpServletRequest request) throws ServletException {
+        request.setAttribute("back-channel-logout", true);
+        request.setAttribute("issuer", issuer);
+        request.setAttribute("token", token);
         request.logout();
+        return "redirect:/model";
     }
 }
--- microservice-library/src/main/java/ru/rt/library/configs/WebSecurityConfig.java
+++ microservice-library/src/main/java/ru/rt/library/configs/WebSecurityConfig.java
@@ -1,31 +1,14 @@
 package ru.rt.library.configs;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.UriComponentsBuilder;
-import ru.rt.library.domain.Token;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.net.URI;
 
 @Configuration
 @EnableWebSecurity
@@ -38,7 +21,7 @@ public class WebSecurityConfig {
             public void configure(HttpSecurity http) throws Exception {
                 http
                         .authorizeRequests()
-                            .antMatchers("/")
+                            .antMatchers("/", "/back-channel-logout")
                             .permitAll()
                             .anyRequest().authenticated()
                             .and()
@@ -48,9 +31,9 @@ public class WebSecurityConfig {
                             .addLogoutHandler(keycloakLogoutHandler)
                             .logoutUrl("/logout")
                             .logoutSuccessUrl("/")
-                            .invalidateHttpSession(true)
+                            //.deleteCookies("JSESSIONID")
                             .clearAuthentication(true)
-                            .deleteCookies("JSESSIONID");
+                            .invalidateHttpSession(true);
             }
         };
     }
@@ -66,89 +49,7 @@ public class WebSecurityConfig {
 
     @Bean
     KeycloakLogoutHandler keycloakLogoutHandler() {
-        return new KeycloakLogoutHandler();
-    }
-}
-
-class KeycloakLogoutHandler extends SecurityContextLogoutHandler {
-
-    private final Logger logger = LoggerFactory.getLogger(KeycloakLogoutHandler.class);
-
-    @Autowired
-    private WebClient webClient;
-
-    @Override
-    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-        super.logout(request, response, authentication);
-//        logoutFromKeyCloak(authentication);
-        logoutFromKeyCloakREST(authentication);
-    }
-
-    private void logoutFromKeyCloak(Authentication authentication) {
-        OidcUser oidcUser = (OidcUser)authentication.getPrincipal();
-        URI logoutUri = UriComponentsBuilder
-                .fromUriString(oidcUser.getIssuer()+"/protocol/openid-connect/logout")
-                .queryParam("id_token_hint", oidcUser.getIdToken().getTokenValue()).build().toUri();
-
-        ResponseEntity<Void> response = this.webClient
-                .get()
-                .uri(logoutUri)
-                .retrieve()
-                .toBodilessEntity()
-                .block();
-
-        if (response != null) {
-            logger.info("Log out response: " + response.getStatusCode());
-        }
-    }
-
-    private void logoutFromKeyCloakREST(Authentication authentication) {
-        OidcUser oidcUser = (OidcUser) authentication.getPrincipal();
-        String s = oidcUser.getIssuer().toString();
-        String beginUri = s.substring(0, s.indexOf("/realms")) + "/admin" + s.substring(s.indexOf("/realms"));
-        String userId = oidcUser.getClaim("sub");
-
-        URI accessTokenUri = UriComponentsBuilder
-                .fromUriString(s + "/protocol/openid-connect/token")
-                .build().toUri();
-
-        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
-        formData.add("username", "tester");
-        formData.add("password", "123456");
-        formData.add("grant_type", "password");
-        formData.add("client_id", "microservice-cinema");
-        formData.add("client_secret", "192f9bc9-7617-4b97-a549-92dc40d091be");
-
-        ResponseEntity<Token> respToken = this.webClient
-                .post()
-                .uri(accessTokenUri)
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .body(BodyInserters.fromFormData(formData))
-                .exchange()
-                .flatMap(response -> response.toEntity(Token.class))
-                .block();
-
-        if (respToken != null) {
-            // REST Logout user example: http://127.0.0.1:8180/auth/admin/realms/heroes/users/83c72e88-7ac9-4fc7-a7fb-97736d67d261/logout
-
-            URI logoutURI = UriComponentsBuilder
-                    .fromUriString(beginUri + "/users/" + userId + "/logout")
-                    .build().toUri();
-
-            ResponseEntity<Void> response = this.webClient
-                    .post()
-                    .uri(logoutURI)
-                    .header("Authorization", "Bearer " + respToken.getBody().accessToken)
-                    .header("Content-Type", "application/json")
-                    .header("Cache-Control", "no-cache")
-                    .retrieve()
-                    .toBodilessEntity()
-                    .block();
-
-            if (response != null) {
-                logger.info("Log out response: " + response.getStatusCode());
-            }
-        }
+        return new KeycloakLogoutHandler("JSESSIONID");
     }
 }
 
