cb39ac6a0f06fb6585df2d7ff31cd661d9e8b54c
Aleksey Вaidin <alekseybaidinq@gmail.com>
2021-07-13 22:12:28 +0500

    Реализован microservice-music; добавлены новые index и subscribe страницы (возможно их стоит объединить?)

--- microservice-music/src/main/java/ru/rt/music/controllers/MusicController.java
+++ microservice-music/src/main/java/ru/rt/music/controllers/MusicController.java
@@ -3,6 +3,7 @@ package ru.rt.music.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -11,9 +12,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.ModelAndView;
 import ru.rt.music.domain.Song;
+import ru.rt.music.handlers.RestRequestHandler;
+import ru.rt.music.services.UserDetailsCollectorService;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,27 +30,64 @@ import java.util.stream.Collectors;
 @RequestMapping
 public class MusicController {
 
-    @Value("${resource-server.api.url}")
-    private String musicApiUrl;
+    @Autowired
+    private UserDetailsCollectorService userDetailsCollectorService;
 
     @Autowired
-    private WebClient webClient;
+    private RestRequestHandler restRequestHandler;
 
-    @GetMapping("/model")
-    public String getAllSongs(Model model) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        DefaultOidcUser p = (DefaultOidcUser) authentication.getPrincipal();
-        model.addAttribute("email", p.getClaims().get("email"));
-        model.addAttribute("authorities",
-                p.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining(", ")));
+    @GetMapping("/")
+    public String mainPage(Model model, Principal principal) {
+        model.addAttribute("principal", principal);
+        model.addAttribute("songs", restRequestHandler.requestToGetAllSongs());
 
-        List<Song> songs = this.webClient.get()
-                .uri(musicApiUrl)
-                .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<List<Song>>() {
-                })
-                .block();
-        model.addAttribute("songs", songs);
-        return "model";
+        return "index";
+    }
+
+    @GetMapping("/account")
+    public String accountPage(Model model, Authentication authentication) {
+        DefaultOidcUser principal = (DefaultOidcUser) authentication.getPrincipal();
+        model.addAttribute("principal", principal);
+        model.addAttribute("userInfo", userDetailsCollectorService.getUserInfo(principal));
+        return "account";
+    }
+
+    @GetMapping("/back-channel-logout")
+    public String logout(@RequestParam String issuer, @RequestParam String token, HttpServletRequest request) throws ServletException {
+        request.setAttribute("back-channel-logout", true);
+        request.setAttribute("issuer", issuer);
+        request.setAttribute("token", token);
+        request.logout();
+        return "redirect:/model";
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("/admin")
+    public ModelAndView accountPage(Principal principal) {
+        return new ModelAndView("admin", Collections.singletonMap("principal", principal));
+    }
+
+    @PreAuthorize("hasRole('ROLE_SUBSCRIBER')")
+    @GetMapping("/subscribe")
+    public String subscribePage(Model model, Authentication authentication) {
+        DefaultOidcUser principal = (DefaultOidcUser) authentication.getPrincipal();
+        model.addAttribute("principal", principal);
+
+        boolean hasSubscriberRole = principal.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .anyMatch(authority -> authority.contains("ROLE_SUBSCRIBER"));
+
+        if (hasSubscriberRole) {
+            model.addAttribute("subscriptionEnd", principal.getUserInfo().getClaim("sub_end"));
+        }
+
+        model.addAttribute("hasSubscriberRole", hasSubscriberRole);
+        model.addAttribute("songs", restRequestHandler.requestToGetAllSongs());
+        return "subscribe";
+    }
+
+    @GetMapping("/about")
+    public ModelAndView aboutPage(Principal principal) {
+        return new ModelAndView("subscribe", Collections.singletonMap("principal", principal));
     }
 }
--- microservice-music/src/main/java/ru/rt/music/domain/Song.java
+++ microservice-music/src/main/java/ru/rt/music/domain/Song.java
@@ -1,7 +1,36 @@
 package ru.rt.music.domain;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Song {
     public Long id;
     public String title;
     public String musicianName;
+    public Double rating;
+    public String duration;
+    public byte[] albumCover;
+    public String filename;
+
+    public void saveCoverImageLocally() {
+        try {
+            Path path = Paths.get("microservice-music/src/main/resources/static/images/" + this.filename + ".jpg");
+            if (Files.notExists(path)) {
+                ByteArrayInputStream is = new ByteArrayInputStream(this.albumCover);
+                BufferedImage image = ImageIO.read(is);
+                ImageIO.write(image, "jpg", path.toFile());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getCoverImageSrc() {
+        return String.format("/static/images/%s.jpg", this.filename);
+    }
 }
