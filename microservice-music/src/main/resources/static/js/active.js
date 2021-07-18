222087bfa27e7387eb1962ce7baf6b9c327c13a3
Aleksey Вaidin <alekseybaidinq@gmail.com>
2021-07-18 14:53:04 +0500

    Рестайлинг music-microservice

--- microservice-music/src/main/resources/static/js/active.js
+++ microservice-music/src/main/resources/static/js/active.js
@@ -1,3 +1,15 @@
-$('body').on('click', '.nav-item', function (e) {
-    //TODO
+$(document).ready(function() {
+    $(".active").removeClass("active");
+    const pageURL = $(location).attr("href");
+    if (pageURL.endsWith("subscribe")) {
+        $("#subscribe-nav-link").addClass("active");
+    } else if (pageURL.endsWith("account")) {
+        $("#account-nav-link").addClass("active");
+    } else if (pageURL.endsWith("authors")) {
+        $("#authors-nav-link").addClass("active");
+    }else if (pageURL.endsWith("admin")) {
+        $("#admin-nav-link").addClass("active");
+    } else {
+        $("#home-nav-link").addClass("active");
+    }
 });
